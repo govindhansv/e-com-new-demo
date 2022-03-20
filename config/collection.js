458cb2aa4f948d524f84df6d1b0de4a0161509b7
govindhansv <govindhansv333@gmail.com>
2022-03-20 08:55:47 +0530

    new

---
+++ config/collection.js
@@ -0,0 +1,19 @@
+module.exports={
+    PRODUCT_COLLECTION:'product',
+    USER_COLLECTION:'user',
+    ADMIN_LOGIN:'admindata',
+    CART_COLLECTION:'cart',
+    ORDER_COLLECTION:'order',
+    ADDRESS_COLLECTION:'useraddress',
+    COUPON_COLLECTION:'coupon',
+    // accountSid : process.env.TWILIO_ACCOUNT_SID,
+    // authToken : process.env.TWILIO_AUTH_TOKEN,
+    // serviceId : process.env.TWILIO_SERVICE_ID,
+    accountSid : 'fgfg',
+    authToken : 'fgfg',
+    serviceId : 'fgfg',
+    keySecret : 'idW13UoRAG6lIfscmZMKtfN8',
+    keyId : 'rzp_test_BRw5z3fOvoSJO8'
+    // keySecret : process.env.RAZORPAY_KEY_SECRET,
+    // keyId : process.env.RAZORPAY_KEY_ID
+}
